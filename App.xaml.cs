73e077b892c730fb90881e41f26b106f59bc3d33
Talha Salman Minhas <talha.salman.minhas@icloud.com>
2023-11-04 23:11:17 +0200

    Project Initialisation and reworking from previous version.

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace InkLink
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace InkLink
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

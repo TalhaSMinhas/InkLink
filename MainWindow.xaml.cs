4c7b2a442161644082d49f284719f4a76e6ee04e
Talha Salman Minhas <talha.salman.minhas@icloud.com>
2023-11-04 23:50:20 +0200

    Dummy data added to code behind, and binding for note name in list view.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace InkLink
 {
@@ -17,6 +18,47 @@ namespace InkLink
         public MainWindow()
         {
             InitializeComponent();
+            
+            var note1 = new Note
+            {
+                NoteName = "Client Meeting Notes",
+                Content = "Discussed project requirements and deadlines with the client. Agreed to finalize the proposal by Friday."
+            };
+
+            var note2 = new Note
+            {
+                NoteName = "Daily Tasks",
+                Content = "1. Buy groceries\n2. Finish report for the marketing team\n3. Call the plumber about the leaky faucet"
+            };
+
+            var note3 = new Note
+            {
+                NoteName = "Project Idea",
+                Content = "Brainstormed a new app concept that could revolutionize the way we take notes. Exciting potential!"
+            };
+
+            var note4 = new Note
+            {
+                NoteName = "Pasta Recipe",
+                Content = "Ingredients:\n- 8 oz pasta\n- 2 cups tomato sauce\n- 1/2 cup grated Parmesan cheese\nInstructions: Boil pasta, mix with sauce, sprinkle cheese, and serve."
+            };
+
+            var note5 = new Note
+            {
+                NoteName = "Inspiration",
+                Content = "In the middle of every difficulty lies opportunity. - Albert Einstein"
+            };
+
+            var notes = new List<Note>
+            {
+                note1,
+                note2,
+                note3,
+                note4,
+                note5
+            };
+
+            NotesList.ItemsSource = notes;
         }
 
         private void BtnClose_OnClick(object sender, RoutedEventArgs e)
